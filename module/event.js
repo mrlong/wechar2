595ade417d20f74e212d9650809fb4a49f59350b
mrlong <mrlong.com@gmail.com>
2014-04-02 11:03:09 +0800

    增加增加用户,删除用户

--- module/event.js
+++ module/event.js
@@ -6,13 +6,28 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+// message为事件内容
+  // { ToUserName: 'gh_d3e07d51b513',
+  // FromUserName: 'oPKu7jgOibOA-De4u8J2RuNKpZRw',
+  // CreateTime: '1359125022',
+  // MsgType: 'event',
+  // Event: 'LOCATION',
+  // Latitude: '23.137466',
+  // Longitude: '113.352425',
+  // Precision: '119.385040',
+  // MsgId: '5837397520665436492' }
+var User = require('../lib/userSchema');
+
 module.exports = function(event, req, res, next){
 	console.log(event);
   if (event.Event === 'subscribe') {
   	// 用户添加时候的消息
   	res.reply('谢谢添加字典公众账号，查生癖字，查字解释，字的祥细注解。应用尽用，新华字典有的都有。:)\n请输入一个字，将会得到相关描述。');
+    //写入库内的人员信息
+    User.add(event.FromUserName,function(err){});
   } else if (event.Event === 'unsubscribe') {
   	res.reply('Bye! 谢谢你的关注下次再来。');
+    User.leave(event.FromUserName,function(err){});
   } else {
   	res.reply('暂未支持! Coming soon!');
   }
