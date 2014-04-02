a40024b979a4b8e1a38348d85788b3a89c2fc917
mrlong <mrlong.com@gmail.com>
2014-04-02 09:41:33 +0800

    增加关注的提示

--- module/event.js
+++ module/event.js
@@ -10,9 +10,9 @@ module.exports = function(event, req, res, next){
 	console.log(event);
   if (event.Event === 'subscribe') {
   	// 用户添加时候的消息
-  	res.reply('谢谢添加龙仕云公共帐号:)\n回复相关关键词，将会得到相关描述。');
+  	res.reply('谢谢添加字典公众账号，查生癖字，查字解释，字的祥细注解。应用尽用，新华字典有的都有。:)\n请输入一个字，将会得到相关描述。');
   } else if (event.Event === 'unsubscribe') {
-  	res.reply('Bye!');
+  	res.reply('Bye! 谢谢你的关注下次再来。');
   } else {
   	res.reply('暂未支持! Coming soon!');
   }
