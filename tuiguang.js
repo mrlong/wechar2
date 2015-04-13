0817209ed2da67d6497538acfecba9c2899d0099
龙仕云 <mrlong.com@gmail.com>
2015-04-13 22:07:30 +0800

    修改：  更新推荐方法
    Signed-off-by: 龙仕云 <mrlong.com@gmail.com>

--- tuiguang.js
+++ tuiguang.js
@@ -9,6 +9,6 @@
 // content 是返回的值，要这个内容插值。
 // fn(err,conent)
 exports.textmessage=function(input,content,fn){
-  content.push({title:'  将本应用推荐给你的好友...',url:'http://xhzd.mrlong.cn'});
+  content.push({title:'====将本应用推荐给我的好友====',url:'http://xhzd.mrlong.cn'});
   if(fn){fn(null,content)};  
 };
